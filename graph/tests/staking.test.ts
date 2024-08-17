9ad03e9428da52a69ce5b10cfbdd06bc470c811e
gulshanpr <gulshankumarprasad04@gmail.com>
2024-08-18 03:41:58 +0530

    deployed to graph

--- graph/tests/staking.test.ts
+++ graph/tests/staking.test.ts
@@ -5,7 +5,7 @@ import {
   clearStore,
   beforeAll,
   afterAll
-} from "matchstick-as/assembly/index"
+} from "matchstick-as"
 import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
 import { Fallback } from "../generated/schema"
 import { Fallback as FallbackEvent } from "../generated/Staking/Staking"
